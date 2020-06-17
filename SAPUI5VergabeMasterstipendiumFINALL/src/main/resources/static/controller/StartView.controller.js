16005aa8c6b287d2317ae6a74e34b909036cd0f8
Patrik Balazs <patrik.balazs@sap.com>
2020-06-17 12:16:18 +0200

    Routing initialized...

---
+++ SAPUI5VergabeMasterstipendiumFINALL/src/main/resources/static/controller/StartView.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.sap.masterstipendium.SAPUI5VergabeMasterstipendiumFrontEnd.controller.StartView", {
+		onInit: function () {
+
+		}
+	});
+});
