f7842b3ffe9a3eeafc67b522f258f6d4e975656c
Patrik Balazs <patrik.balazs@sap.com>
2020-06-22 10:37:03 +0200

    ... hlo

--- SAPUI5VergabeMasterstipendiumFINALL/src/main/resources/static/controller/StudentFormView.controller.js
+++ SAPUI5VergabeMasterstipendiumFINALL/src/main/resources/static/controller/StudentFormView.controller.js
@@ -28,6 +28,11 @@ sap.ui.define([
 			}
 			
 			
+		},
+		
+		onNavBack: function(){
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("ausbilderView", true);
 		}
 });
 	
